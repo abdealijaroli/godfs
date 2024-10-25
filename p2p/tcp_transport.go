5942ce4a2547dc8ca5fb6d58a2c6b1074d5598ed
abdealijaroli <abdealiburu03@gmail.com>
2024-10-25 22:31:14 +0530

    Refactor TCPTransport

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -2,11 +2,35 @@ package p2p
 
 import (
 	"net"
+	"sync"
 )
 
 type TCPTransport struct {
 	listenAddr string
 	listener   net.Listener
 
-	// peers map[string]
+	mu    sync.RWMutex
+	peers map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		listenAddr: listenAddr,
+		peers:      make(map[net.Addr]Peer),
+	}
+}
+
+func (t *TCPTransport) Start() error {
+    listener, err := net.Listen("tcp", t.listenAddr)
+    if err != nil {
+        return err
+    }
+
+    t.listener = listener
+    go t.acceptConnections()
+
+    return nil
+}
+
+func (t *TCPTransport) Stop() error {
 }
