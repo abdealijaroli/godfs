457e1d95f24512745669935209311a9630c43e0a
abdealijaroli <abdealiburu03@gmail.com>
2024-11-25 09:36:42 +0530

    Refactor import path for p2p package and clean up main function

--- main.go
+++ main.go
@@ -3,18 +3,14 @@ package main
 import (
 	"log"
 
-	"github.com/abdealijaroli/godfs/p2p"
+	"github.com/abdealijaroli/godfs/pkg/p2p"
 )
 
 func main() {
-	// create a new TCP transport
 	t := p2p.NewTCPTransport(":8080")
 
-	// start listeningn for incoming connections
 	err := t.ListenAndAccept()
 	if err != nil {
 		log.Fatal(err)
-	}
-	for {
 	}
 }
