088a15a80bafb94941d710880b295f8b67edee15
abdealijaroli <abdealiburu03@gmail.com>
2025-01-02 19:31:40 +0530

    feat: implement consistent hashing for DHT node management

--- internal/node/dht.go
+++ internal/node/dht.go
@@ -2,7 +2,9 @@ package node
 
 import (
 	"errors"
+	"hash/fnv"
 	"log"
+	"sort"
 	"sync"
 )
 
@@ -67,6 +69,41 @@ func (d *DHT) Put(key, value string, replicationFactor int) error {
 	return nil
 }
 
+func (d *DHT) PutConsistent(key, value string, replicationFactor int) error {
+	targetNode := d.consistentHash(key)
+	if targetNode == "" {
+		return errors.New("no nodes available")
+	}
+
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	err := d.sendToNode(targetNode, key, value)
+	if err != nil {
+		log.Printf("Failed to replicate key %s to node %s: %v", key, targetNode, err)
+		return err
+	}
+
+	// Also store locally
+	d.data[key] = value
+
+	// Replicate to additional nodes based on replication factor
+	replicated := 1
+	for _, node := range d.nodes {
+		if node == targetNode || replicated >= replicationFactor {
+			break
+		}
+		err := d.sendToNode(node, key, value)
+		if err != nil {
+			log.Printf("Failed to replicate key %s to node %s: %v", key, node, err)
+			continue
+		}
+		replicated++
+	}
+
+	return nil
+}
+
 func (d *DHT) Get(key string) (string, error) {
 	d.lock.RLock()
 	defer d.lock.RUnlock()
@@ -76,15 +113,9 @@ func (d *DHT) Get(key string) (string, error) {
 		return value, nil
 	}
 
-	for _, node := range d.nodes {
-		if node == d.selfNode {
-			continue
-		}
-
-		value, err := d.queryNode(node, key)
-		if err == nil {
-			return value, nil
-		}
+	targetNode := d.consistentHash(key)
+	if targetNode != "" {
+		return d.queryNode(targetNode, key)
 	}
 
 	return "", errors.New("key not found in DHT or replicas")
@@ -110,12 +141,39 @@ func (d *DHT) ListNodes() []string {
 
 func (d *DHT) sendToNode(node, key, value string) error {
 	log.Printf("Replicating key %s to node %s", key, node)
-
+	// todo
 	return nil
 }
 
 func (d *DHT) queryNode(node, key string) (string, error) {
 	log.Printf("Querying key %s from node %s", key, node)
-
+	// todo
 	return "", errors.New("not implemented")
+}
+
+// consistent hashing
+func hash(key string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return h.Sum32()
+}
+
+func (d *DHT) consistentHash(key string) string {
+	if len(d.nodes) == 0 {
+		return ""
+	}
+
+	keyHash := hash(key)
+
+	sort.Slice(d.nodes, func(i, j int) bool {
+		return hash(d.nodes[i]) < hash(d.nodes[j])
+	})
+
+	for _, node := range d.nodes {
+		if hash(node) >= keyHash {
+			return node
+		}
+	}
+
+	return d.nodes[0]
 }
