05bbf278d73c849d4b99edc9cac7d358ddfca96f
abdealijaroli <abdealiburu03@gmail.com>
2025-01-16 12:30:16 +0530

    feat: update chunk ID and checksum to use hex encoding for improved readability

--- internal/file/chunk.go
+++ internal/file/chunk.go
@@ -1,49 +1,50 @@
 package file
 
 import (
-	"crypto/sha256"
-	"io"
-	"os"
+    "crypto/sha256"
+    "encoding/hex"
+    "io"
+    "os"
 )
 
 type Chunk struct {
-	ID       string
-	Data     []byte
-	Checksum string
+    ID       string
+    Data     []byte
+    Checksum string
 }
 
 func SplitFile(filepath string, chunkSize int) ([]Chunk, error) {
-	file, err := os.Open(filepath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
+    file, err := os.Open(filepath)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
 
-	var chunks []Chunk
-	buffer := make([]byte, chunkSize)
+    var chunks []Chunk
+    buffer := make([]byte, chunkSize)
 
-	for {
-		n, err := file.Read(buffer)
-		if err != nil && err != io.EOF {
-			return nil, err
-		}
-		if n == 0 {
-			break
-		}
+    for {
+        n, err := file.Read(buffer)
+        if err != nil && err != io.EOF {
+            return nil, err
+        }
+        if n == 0 {
+            break
+        }
 
-		data := buffer[:n]
-		checksum := sha256.Sum256(data)
-		chunk := Chunk{
-			ID:       string(checksum[:8]),
-			Data:     data,
-			Checksum: string(checksum[:]),
-		}
-		chunks = append(chunks, chunk)
-	}
-	return chunks, nil
+        data := buffer[:n]
+        checksum := sha256.Sum256(data)
+        chunk := Chunk{
+            ID:       hex.EncodeToString(checksum[:8]), // Use hex encoding for the chunk ID
+            Data:     data,
+            Checksum: hex.EncodeToString(checksum[:]), // Use hex encoding for the checksum
+        }
+        chunks = append(chunks, chunk)
+    }
+    return chunks, nil
 }
 
 func VerifyChecksum(chunk Chunk) bool {
-	computed := sha256.Sum256(chunk.Data)
-	return string(computed[:]) == chunk.Checksum
+    computed := sha256.Sum256(chunk.Data)
+    return hex.EncodeToString(computed[:]) == chunk.Checksum
 }
