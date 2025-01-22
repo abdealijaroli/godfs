259c353b27a5630b0f265fc1a378a1d01b398e85
abdealijaroli <abdealiburu03@gmail.com>
2025-01-22 09:58:54 +0530

    feat: add TLS configuration support to DHT tests for secure connections

--- test/dht_test.go
+++ test/dht_test.go
@@ -1,13 +1,43 @@
 package node_test
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"os"
 	"testing"
 
 	"github.com/abdealijaroli/godfs/internal/node"
 )
 
+func loadTLSConfig() (*tls.Config, error) {
+	cert, err := tls.LoadX509KeyPair("../certs/server.crt", "../certs/server.key")
+	if err != nil {
+		return nil, err
+	}
+
+	caCert, err := os.ReadFile("../certs/ca.crt")
+	if err != nil {
+		return nil, err
+	}
+
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      caCertPool,
+	}
+
+	return tlsConfig, nil
+}
+
 func TestDHT(t *testing.T) {
-	dht := node.NewDHT("node1")
+	tlsConfig, err := loadTLSConfig()
+	if err != nil {
+		t.Fatalf("Failed to load TLS config: %v", err)
+	}
+
+	dht := node.NewDHT("node1", tlsConfig)
 
 	dht.AddNode("node2")
 	dht.AddNode("node3")
@@ -30,17 +60,26 @@ func TestDHT(t *testing.T) {
 }
 
 func TestDHTReplication(t *testing.T) {
-	dht := node.NewDHT("node1")
+	tlsConfig, err := loadTLSConfig()
+	if err != nil {
+		t.Fatalf("Failed to load TLS config: %v", err)
+	}
+
+	dht := node.NewDHT("node1", tlsConfig)
+
 	dht.AddNode("node2")
 	dht.AddNode("node3")
 
-	err := dht.PutConsistent("file1", "chunk1_data", 2)
+	dht.Put("file1", "chunk1_location", 3)
+
+	// Simulate replication
+	err = dht.Replicate("file1", "chunk1_location")
 	if err != nil {
-		t.Fatalf("Failed to store data in DHT: %s", err)
+		t.Fatalf("Failed to replicate key: %s", err)
 	}
 
 	value, err := dht.Get("file1")
-	if err != nil || value != "chunk1_data" {
-		t.Fatalf("Expected chunk1_data, got %s", value)
+	if err != nil || value != "chunk1_location" {
+		t.Fatalf("Expected chunk1_location, got %s", value)
 	}
 }
