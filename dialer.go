d1711261927ead6b10cb654bc901df26e1cc37c7
abdealijaroli <abdealiburu03@gmail.com>
2025-02-11 22:27:20 +0530

    refactor: comment out unused code in dialer.go

--- dialer.go
+++ dialer.go
@@ -2,43 +2,43 @@ package main
 
 import (
     "log"
-    "time"
+//     "time"
 
-    "github.com/abdealijaroli/godfs/config"
-    "github.com/abdealijaroli/godfs/pkg/p2p"
+//     "github.com/abdealijaroli/godfs/config"
+//     "github.com/abdealijaroli/godfs/pkg/p2p"
 )
 
 func Dial() {
-    tlsConfig, err := config.LoadTLSConfig("certs/client.crt", "certs/client.key", "certs/ca.crt")
-    if err != nil {
-        log.Fatal("Failed to load TLS config:", err)
-    }
+    // tlsConfig, err := config.LoadTLSConfig("certs/client.crt", "certs/client.key", "certs/ca.crt")
+    // if err != nil {
+    //     log.Fatal("Failed to load TLS config:", err)
+    // }
 
-    transport := p2p.NewTCPTransport(":8081", tlsConfig)
+    // transport := p2p.NewTCPTransport(":8081", tlsConfig)
 
-    go func() {
-        if err := transport.ListenAndAccept(); err != nil {
-            log.Fatal(err)
-        }
-    }()
+    // go func() {
+    //     if err := transport.ListenAndAccept(); err != nil {
+    //         log.Fatal(err)
+    //     }
+    // }()
 
-    time.Sleep(2 * time.Second)
+    // time.Sleep(2 * time.Second)
 
-    log.Println("Dialing server at localhost:8080")
-    peer, err := transport.Dial("localhost:8080")
-    if err != nil {
-        log.Fatal("Failed to connect to server:", err)
-    }
+    // log.Println("Dialing server at localhost:8080")
+    // peer, err := transport.Dial("localhost:8080")
+    // if err != nil {
+    //     log.Fatal("Failed to connect to server:", err)
+    // }
 
-    err = peer.Send(p2p.Message{
-        Type:    "greeting",
-        Payload: []byte("Hello from the client node!"),
-    })
-    if err != nil {
-        log.Fatal("Failed to send message:", err)
-    }
+    // err = peer.Send(p2p.Message{
+    //     Type:    "greeting",
+    //     Payload: []byte("Hello from the client node!"),
+    // })
+    // if err != nil {
+    //     log.Fatal("Failed to send message:", err)
+    // }
 
     log.Println("Message sent successfully!")
 
-    select {}
+    // select {}
 }
