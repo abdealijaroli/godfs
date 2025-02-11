3a939ec7acb2bcff98e226972e3096ac26065d8f
abdealijaroli <abdealiburu03@gmail.com>
2025-02-11 22:27:36 +0530

    refactor: comment out unused imports and code in server.go

--- server.go
+++ server.go
@@ -2,31 +2,31 @@ package main
 
 import (
     "log"
-    "os"
-    "os/signal"
-    "syscall"
+    // "os"
+    // "os/signal"
+    // "syscall"
 
-    "github.com/abdealijaroli/godfs/config"
-    "github.com/abdealijaroli/godfs/pkg/p2p"
+    // "github.com/abdealijaroli/godfs/config"
+    // "github.com/abdealijaroli/godfs/pkg/p2p"
 )
 
 func Serve() {
-    tlsConfig, err := config.LoadTLSConfig("certs/server.crt", "certs/server.key", "certs/ca.crt")
-    if err != nil {
-        log.Fatal("Failed to load TLS config:", err)
-    }
+    // tlsConfig, err := config.LoadTLSConfig("certs/server.crt", "certs/server.key", "certs/ca.crt")
+    // if err != nil {
+    //     log.Fatal("Failed to load TLS config:", err)
+    // }
 
-    t := p2p.NewTCPTransport(":8080", tlsConfig)
+    // t := p2p.NewTCPTransport(":8080", tlsConfig)
 
-    go func() {
-        if err := t.ListenAndAccept(); err != nil {
-            log.Printf("Transport error: %v", err)
-        }
-    }()
+    // go func() {
+    //     if err := t.ListenAndAccept(); err != nil {
+    //         log.Printf("Transport error: %v", err)
+    //     }
+    // }()
 
-    sigChan := make(chan os.Signal, 1)
-    signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-    <-sigChan
+    // sigChan := make(chan os.Signal, 1)
+    // signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+    // <-sigChan
 
     log.Println("Shutting down server...")
 }
