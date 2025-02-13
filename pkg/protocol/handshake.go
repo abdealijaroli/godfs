da6f6fb4766028c87ef4eb29f84a58cefc59bf77
abdealijaroli <abdealiburu03@gmail.com>
2025-02-13 11:06:58 +0530

    refactor: enhance handshake function with improved error handling and logging

--- pkg/protocol/handshake.go
+++ pkg/protocol/handshake.go
@@ -1,29 +1,32 @@
 package protocol
 
 import (
-    "encoding/json"
-    "net"
+	"encoding/json"
+	"net"
+	"fmt"
+	"log"
 )
 
 type Handshake struct {
-    NodeID string
+	NodeID string
 }
 
 func PerformHandshake(conn net.Conn, nodeID string) error {
-    encoder := json.NewEncoder(conn)
-    decoder := json.NewDecoder(conn)
+	hs := Handshake{NodeID: nodeID}
 
-    err := encoder.Encode(Handshake{NodeID: nodeID})
-    if err != nil {
-        return err
-    }
+	// Send handshake
+	encoder := json.NewEncoder(conn)
+	if err := encoder.Encode(hs); err != nil {
+		return fmt.Errorf("failed to send handshake: %v", err)
+	}
 
-    var response Handshake
-    err = decoder.Decode(&response)
-    if err != nil {
-        return err
-    }
+	// Read response
+	decoder := json.NewDecoder(conn)
+	var response Handshake
+	if err := decoder.Decode(&response); err != nil {
+		return fmt.Errorf("failed to read handshake response: %v", err)
+	}
 
-    println("Handshake successful with node:", response.NodeID)
-    return nil
+	log.Printf("Handshake successful with node: %s", response.NodeID)
+	return nil
 }
