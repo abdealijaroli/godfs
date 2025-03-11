ab51d7d192f425901c6cae510b5be13de75a4eaf
Abdeali Jaroli <abdealiburu03@gmail.com>
2025-03-11 10:44:53 +0530

    fix: dev

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ type DebugServer struct {
 	fileManager *file.FileManager
 }
 
+// For dev purposes, we are not using TLS
 func NewDebugServer(dht *node.DHT, fileManager *file.FileManager) *DebugServer {
 	return &DebugServer{dht: dht, fileManager: fileManager}
 }
